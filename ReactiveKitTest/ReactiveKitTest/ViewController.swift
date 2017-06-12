96178ab173bfb6d8736b24c19e88ce786219fe38
LinJian <linjian@ling.ai>
2017-06-12 15:23:15 +0800

    init.

---
+++ ReactiveKitTest/ReactiveKitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactiveKitTest
+//
+//  Created by 小白 on 2017/6/12.
+//  Copyright © 2017年 LinJian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
